2d0474b3151d733e05cf0e5d7eebba5409e864e3
Exudias <editoo31@gmail.com>
2024-07-04 20:16:01 +0300

    Improve display of input

--- src/components/ResultPanel.jsx
+++ src/components/ResultPanel.jsx
@@ -3,33 +3,45 @@ import '../styles/ResultPanel.css';
 function ResultPanel({name, email, phone, education, experience}) 
 {
     return <div id="result">
-        <h1>General</h1>
+        <div>
+            <h1>{name}</h1>
+            <div className='result-contacts'>
+                <span>{email}</span>
+                <span>{phone}</span>
+            </div>
+        </div>
         <hr></hr>
-        <h1>{name}</h1>
-        <h1>{email}</h1>
-        <h1>{phone}</h1>
+        {education.length > 0 && 
+        <>
+        <div className="result-heading">
+            <h1>Education</h1>
+        </div>
         <hr></hr>
-        <h1>Education</h1>
-        <hr></hr>
-        <>{education.length > 0 && education.map((e, index) => 
-            <div key={index}>
+        <>{education.map((e, index) => 
+            <div key={index} className='result-section'>
                 <h3>{e.name}</h3>
                 <h4>{e.title}</h4>
                 <h4>{e.duration}</h4>
             </div>
         )}</>
         <hr></hr>
-        <h1>Experience</h1>
-        <hr></hr>
-        <>{experience.length > 0 && experience.map((e, index) => 
-            <div key={index}>
-                <h3>{e.name}</h3>
-                <h4>{e.title}</h4>
-                <h4>{e.responsibilities}</h4>
-                <h4>{e.duration}</h4>
+        </>}
+        {experience.length > 0 &&
+        <>
+            <div className="result-heading">
+                <h1>Experience</h1>
             </div>
-        )}</>
-        <hr></hr>
+            <hr></hr>
+            <>{experience.map((e, index) => 
+                <div key={index} className='result-section'>
+                    <h3>{e.name}</h3>
+                    <h4>{e.title}</h4>
+                    <h4>{e.responsibilities}</h4>
+                    <h4>{e.duration}</h4>
+                </div>
+            )}</>
+        </>
+        }
     </div>
 }
 
